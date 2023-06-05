938da596d991851af53649f3a39371aa59e745cd
unknown <guptaatin.123@gmail.com>
2023-06-05 14:12:09 +0530

    chess bg added

---
+++ macrotechtitan/static/js/macrotechtitan.js
@@ -0,0 +1,84 @@
+var parentElem = document.getElementById('maincontainer').parentNode;
+var parentWidth = parentElem.offsetWidth;
+var parentHeight = parentElem.offsetHeight;
+var howmanyboxes=parentWidth/64;
+console.log(parentWidth + "--"+ parentHeight + "---" + Math.ceil(howmanyboxes));
+var createBoard = function()
+{
+    rank = ["A","B","C","D","E","F","G","H" ,"I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    file = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22];
+    var currentTile = {};
+    var count = 0;
+    for(var i = 0; i < rank.length; i++)
+    // console.log(rank.length);
+    {
+        for (var j = 0; j < file.length; j++)
+        {
+            if(count%18 == 0)
+            {
+            $("#chessboard").append('<div class="tile smoothfade" style="clear:left; position:relative;"><div class="circle"></div></div>');
+
+            }else{
+            $("#chessboard").append('<div class="tile smoothfade" style="position:relative;"><div class="circle"></div></div>');
+            }
+            $(".tile").eq(count).attr("data-gridpos",((rank[rank.length - (i +1)]+file[j])));
+            
+            if(((i%2 == 0) && (j%2 != 0)) || ((i%2 != 0) && (j%2 == 0)))
+            {
+            $(".tile").eq(count).addClass("black"  + " " + rank[rank.length - (i +1)]+file[j]);
+            }
+            else
+            {
+            $(".tile").eq(count).addClass("white"  + " " + rank[rank.length - (i +1)]+file[j]);
+            }
+            count++;
+        }
+    }
+}
+jQuery(document).ready(function($) {
+    createBoard();
+    console.log("Created Chessboard");
+    tiles = $(".tile");
+    $(".tile").on('click',function(){
+        currentTile = $(this);
+        var midY = $(this).position().top += ( $(this).width() / 2 );
+        var midX = $(this).position().left += ( $(this).width() / 2 );
+        var player = $("#player");
+        tiles.removeClass('legal');		
+        console.log("X : "+midX + " || Y : "+midY);
+        player.css({"top":midY - (0.5 * player.width()), "left":midX-(0.5*player.width())});
+        console.log("Last Clicked Tile : "+currentTile.data("gridpos"));
+
+    })
+    .on('mouseenter',function(){
+        $(this).addClass('hover');
+    })
+    .on('mouseleave',function(){
+        $(this).removeClass('hover');
+    });
+    var displaylegal = $("#controlbox > ul > li");
+    displaylegal.on('click',function(){
+        console.log($(this).attr('id'))
+        legalMove($(this).attr('id'));
+    })
+    $('*[data-gridpos="A1"]').trigger('click');
+});
+// custom jquery
+jQuery(document).ready(function($) {
+    $('.H8 > div').removeClass("circle");
+    $('.G8 > div').removeClass("circle");
+    $('.F8 > div').removeClass("circle");
+    $('.E8 > div').removeClass("circle");
+    $('.D8 > div').removeClass("circle");
+    $('.C8 > div').removeClass("circle");
+    $('.B8 > div').removeClass("circle");
+    $('.A8 > div').removeClass("circle");    
+    $('.A8 > div').removeClass("circle");
+    $('.A7 > div').removeClass("circle");
+    $('.A6 > div').removeClass("circle");
+    $('.A5 > div').removeClass("circle");
+    $('.A4 > div').removeClass("circle");
+    $('.A3 > div').removeClass("circle");
+    $('.A2 > div').removeClass("circle");
+    $('.A1 > div').removeClass("circle");
+});
